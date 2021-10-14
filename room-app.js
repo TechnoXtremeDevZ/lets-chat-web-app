9a582cd0bbb732840fbff133f435a8f88071aff9
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2021-10-13 17:18:41 -0700

    Update room-app.js
    + Initialized Firebase

--- room-app.js
+++ room-app.js
@@ -1,1 +1,13 @@
-﻿
+﻿const firebaseConfig = {
+    apiKey: "AIzaSyABBONcZWGCTWizpQ8Fp2VIIobCACHlTcQ",
+    authDomain: "let-s-chat-app-319fd.firebaseapp.com",
+    databaseURL: "https://let-s-chat-app-319fd-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-app-319fd",
+    storageBucket: "let-s-chat-app-319fd.appspot.com",
+    messagingSenderId: "335903057718",
+    appId: "1:335903057718:web:8c0c5b4e7cd9379a32f597",
+    measurementId: "G-QTS9KYVNKP"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
