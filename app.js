d00e95a518a23662b0fa6793a83c9879b25c5289
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2021-10-13 17:09:23 -0700

    Add files via upload
    + Making function "addUser"
    + Storing username in local storage
    + Redirecting user to room page

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿/* Function to store the username into the system and
+   redirect the user to the room page.
+*/
+
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "web-app-room.html";
+}
