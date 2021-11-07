d398851ce1f61c91a54d3edf59920b94f4060337
TechnoXtremeDevZ <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2021-11-07 11:08:53 -0800

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,10 @@ function addUser() {
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
 
+    firebase.database().ref("/").child(user_name).update({
+        user_name: user_name,
+        purpose: "adding user"
+    })
+
     window.location = "web-app-room.html";
 }
